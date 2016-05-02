32f27c1de6898d69fe8aeac48f670292441d0276
Paulo Menezes <phgm@cesar.org.br>
2016-05-01 23:54:07 -0300

    Scanline fill

--- js/draw.js
+++ js/draw.js
@@ -1,10 +1,10 @@
 var camera = {
 	C: { x: 0, y: -500, z: 500 },
-	N: { x: 10, y: 10, z: 200 },
-	V: { x: 0, y: 0, z: 10 },
-	d: 80,
-	hX: 1,
-	hY: 1
+	N: { x: 0, y: 1, z: -1 },
+	V: { x: 0, y: -1, z: -1 },
+	d: 5,
+	hX: 2,
+	hY: 2
 };
 
 function initialize () {
@@ -63,33 +63,40 @@ function coordenadaVista (ponto) {
 	// ortogonalizar V
 	var prod = produtoEscalar(camera.V, camera.N) / produtoEscalar(camera.N, camera.N);
 	var vLinha = new Point(camera.V.x - prod * camera.N.x, camera.V.y - prod * camera.N.y, camera.V.z - prod * camera.N.z);
+
+	camera.N = normalizar(camera.N);
+	vLinha = normalizar(vLinha);
+
 	// U = N x V'
 	var U = {
-		x: camera.N.y * camera.V.z - camera.N.z * camera.V.y, 
-		y: camera.N.z * camera.V.x - camera.N.x * camera.V.z, 
-		z: camera.N.x * camera.V.y - camera.N.y * camera.V.x
+		x: camera.N.y * vLinha.z - camera.N.z * vLinha.y, 
+		y: camera.N.z * vLinha.x - camera.N.x * vLinha.z, 
+		z: camera.N.x * vLinha.y - camera.N.y * vLinha.x
 	};
 	// Normalização
 	var alpha = {
 		x: normalizar(U),
-		y: normalizar(vLinha),
-		z: normalizar(camera.N)
+		y: vLinha,
+		z: camera.N
 	};
 	var matrizTransformacao = [
 		[alpha.x.x, alpha.x.y, alpha.x.z],
 		[alpha.y.x, alpha.y.y, alpha.y.z],
 		[alpha.z.x, alpha.z.y, alpha.z.z]
 	];
-	if (!b) {
+	if (b) {
 		console.log(U);
 		console.log(alpha.x);
 		console.log(matrizTransformacao);
 		b = true;
 	}
 
-	var subPonto = subtracaoPontos(ponto, camera.C);
+	var subPonto = subtracaoPontos(camera.C, ponto);
 	return multiplicarMatriz(matrizTransformacao, [[subPonto.x],[subPonto.y],[subPonto.z]]);
 }
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
 
 function draw (data) {
 	try {
@@ -98,53 +105,133 @@ function draw (data) {
 			var vLen = data[0].split(' ')[0];
 			var tLen = data[0].split(' ')[1];
 
+			console.log(vLen, tLen, data.length);
+
 			for (var i = 1; i <= parseInt(vLen); i++) {
 				vertices.push(data[i].split(' '));
 			}
 
-			for (var i = parseInt(vLen) + 1; i < data.length - 1; i++) {
+			for (var i = parseInt(vLen) + 1; i < data.length; i++) {
 				triangulos.push(data[i].split(' '));
 			}
 		}
 
-		/*var minX = parseFloat(vertices[0][0]);
-		var maxX = parseFloat(vertices[0][0]);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		var minY = parseFloat(vertices[0][1]);
-		var maxY = parseFloat(vertices[0][1]);
-
-		for (var i = 0; i < vertices.length; i++) {
-			if (parseFloat(vertices[i][0]) > maxX) maxX = parseFloat(vertices[i][0]);
-			if (parseFloat(vertices[i][0]) < minX) minX = parseFloat(vertices[i][0]);
-			
-			if (parseFloat(vertices[i][1]) > maxY) maxY = parseFloat(vertices[i][1]);
-			if (parseFloat(vertices[i][1]) < minY) minY = parseFloat(vertices[i][1]);
-		}*/
-
-		var canvas = document.getElementById("canvas");
-		var ctx = canvas.getContext('2d');
-		ctx.clearRect(0, 0, 500, 500);
-
-		for (var i = 0; i < triangulos.length; i++) {
+		console.log(vertices.length, triangulos.length);
+		for (var i = 0; i < 2; i++) {
 			var verticesTriangulo = [
 				[vertices[triangulos[i][0] - 1][0], vertices[triangulos[i][0] - 1][1], vertices[triangulos[i][0] - 1][2]], 
 				[vertices[triangulos[i][1] - 1][0], vertices[triangulos[i][1] - 1][1], vertices[triangulos[i][1] - 1][2]], 
 				[vertices[triangulos[i][2] - 1][0], vertices[triangulos[i][2] - 1][1], vertices[triangulos[i][2] - 1][2]]
 			];
 
+			console.log('i', i);
+
+			var triV = [];
+
 			for (var j = 0; j < verticesTriangulo.length; j++) {
 				var vista = coordenadaVista(new Point(verticesTriangulo[j][0], verticesTriangulo[j][1], verticesTriangulo[j][2]));
 				
 				var Xs = (camera.d / camera.hX) * (vista[0][0] / vista[2][0]);
-				var Ys = (camera.d / camera.hY) * (vista[1][0] / vista[2][0]);
-				
+				var Ys = (camera.d / camera.hY) * (vista[1][0] / vista[2][0]);				
+
+				var Xs1 = Xs / camera.hX;
+				var Ys1 = Ys / camera.hY;
+
+				var outroPonto = normalizar(new Point(Xs, Ys, 0));
+
+				var xi = (((Xs + 1) / 2) * 500);
+				var xj = (500 - ((Ys + 1) / 2) * 500);
+
+				//console.log('i', Xs, Xs1, outroPonto.x, outroPonto.y, xi, xj);
+
+				triV.push({
+					x: xi, 
+					y: xj
+				});
+			}
+
+			if (triV[0].y > triV[1].y) swap(triV, 0, 1);
+			if (triV[0].y > triV[2].y) swap(triV, 0, 2);
+			if (triV[1].y > triV[2].y) swap(triV, 1, 2);
+
+			var a1 = 0;
+			var a2 = 0;
+			if (triV[1].y == triV[2].y) {
+				a1 = (triV[2].y - triV[0].y) / (triV[2].x - triV[0].x);
+				a2 = (triV[1].y - triV[0].y) / (triV[1].x - triV[0].x);
+
+				drawTopBottom(triV, a1, a2);
+			} else if (triV[0].y == triV[1].y) {
+				a1 = (triV[0].y - triV[2].y) / (triV[0].x - triV[2].x);
+				a2 = (triV[1].y - triV[2].y) / (triV[1].x - triV[2].x);
+
+				drawBottomTop(triV, a1, a2);
+			} else {
+				var v4x = (triV[0].x + ((triV[1].y - triV[0].y) / (triV[2].y - triV[0].y)) * (triV[2].x - triV[0].x));
+
+				a1 = (triV[1].y - triV[0].y) / (triV[1].x - triV[0].x);
+				a2 = (triV[1].y - triV[0].y) / (v4x - triV[0].x);
+
+				drawTopBottom(triV, a1, a2);
+
+				a1 = (triV[0].y - triV[2].y) / (triV[0].x - triV[2].x);
+				a2 = (triV[1].y - triV[2].y) / (triV[1].x - triV[2].x);
+
+				drawBottomTop(triV, a1, a2);
+			}
+
+			ctx.fillStyle = '#FFF';
+			for (var j = 0; j < triV.length; j++) {
+				console.log(triV);
 				ctx.beginPath();
-				ctx.fillStyle = '#FFF';
-				ctx.fillRect(Xs, Ys, 1, 1); 
-				ctx.stroke();
+				ctx.fillRect(triV[j].x, triV[j].y, 1, 1);
 			}
 		}
 	} catch (error) {
+		console.log(error);
 		alert('Arquivo inválido');
 	}
+}
+
+function drawTopBottom (triV, a1, a2) {
+	var xMin = triV[0].x;
+	var xMax = triV[0].x;
+
+	ctx.fillStyle = getRndColor();
+	for (var j = triV[0].y; j < triV[1].y; j++) {
+		for (var k = 1 / a1 + xMin; k < 1 / a2 + xMax; k++) {
+			ctx.fillRect(k, j, 1, 1);
+		}
+		xMin = 1 / a1 + xMin;
+		xMax = 1 / a2 + xMax;
+	}
+}
+
+function drawBottomTop (triV, a1, a2) {
+	var xMin = triV[2].x;
+	var xMax = triV[2].x;
+
+	ctx.fillStyle = getRndColor();
+	for (var j = triV[2].y; j >= triV[1].y; j--) {
+		for (var k = xMax; k >= xMin; k--) {
+			ctx.fillRect(k, j, 1, 1);
+		}
+		xMin -= 1 / a2;
+		xMax -= 1 / a1;
+	}
+}
+
+function swap (array, i, j) {
+	var aux = array[i];
+	array[i] = array[j];
+	array[j] = aux;
+}
+
+function getRndColor() {
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
 }
